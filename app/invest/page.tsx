d22ec43e41c45b469fd2a724011da26f9fb4a298
“Abdul <abdullatiftalal1@gmail.com>
2024-06-29 15:38:40 +0400

    Updates

--- app/invest/page.tsx
+++ app/invest/page.tsx
@@ -1,12 +1,31 @@
-import { EnquireForm } from '@/app/components';
 import Image from 'next/image';
+import { Metadata } from 'next';
+import { EnquireForm, Loading } from '@/app/components';
+import { Investment } from '@/app/types';
+import { fetchData, getFieldValueByName } from '@/app/constants';
+
+const investment: Investment = await fetchData(5);
+
+export async function generateMetadata(): Promise<Metadata> {
+    return {
+        title: investment.header[0].metatitle,
+        description: investment.header[0].metadescription,
+        keywords: investment.header[0].metakeyword
+    };
+}
 
 export default async function InvestPage() {
+    if (!investment || investment === undefined) {
+        return <Loading />;
+    }
     return (
         <div className='mx-auto mb-[30px] w-full sm:mb-[60px] xl:mb-[84px] 3xl:max-w-[1200px]'>
             <div className='relative mb-[82px]'>
                 <div className='text-[40px] font-[700] leading-[38px] small:text-[69px] small:leading-[88px]'>
-                    <h2>Investment</h2>
+                    <h1 className='inline-block max-w-[220px] capitalize small:max-w-[270px]'>
+                        {investment?.['investment-section-1'][0].field_value ??
+                            ''}
+                    </h1>
                     <div className='origin-center-right pointer-events-none absolute top-[50%] z-[-30] translate-x-[10%] translate-y-[-55%] scale-[1.1] md:translate-x-[20%] md:scale-[1.1] lg:translate-x-[15%] lg:translate-y-[-75%] lg:scale-[1.1] xl:translate-x-[5%] 2xl:scale-[1.3] mdlap:translate-y-[-60%]'>
                         <Image
                             src='/images/invest_building.webp'
@@ -17,38 +36,18 @@ export default async function InvestPage() {
                     </div>
                 </div>
             </div>
-            <div className='mt:[50px] flex flex-col flex-nowrap gap-[30px] sm:mt-[150px] lg:mt-[80px] lg:flex-row lg:flex-wrap xl:mt-[220px]'>
-                <p className='text-xs leading-[162%] lg:w-[calc(50%-15px)]'>
-                    Contrary to popular belief, Lorem Ipsum is not simply random
-                    text. It has roots in a piece of classical Latin literature
-                    from 45 BC, making it over 2000 years old. Richard
-                    McClintock, a Latin professor at Hampden-Sydney College in
-                    Virginia, looked up one of the more obscure Latin words,
-                    consectetur, from a Lorem Ipsum passage, and going through
-                    the cites of the word in classical literature, discovered
-                    the undoubtable source. Lorem Ipsum comes from sections
-                    1.10.32 and 1.10.33 of &quot;de Finibus Bonorum et
-                    Malorum&quot; (The Extremes of Good and Evil) by Cicero,
-                    written in 45 BC. This book is a treatise on the theory of
-                    ethics, very popular during the Renaissance. The first line
-                    of Lorem Ipsum, &quot;Lorem ipsum dolor sit amet..&quot;,
-                    comes from a line in section 1.10.32.
-                </p>
-                <p className='text-xs leading-[162%] lg:w-[calc(50%-15px)]'>
-                    The standard chunk of Lorem Ipsum used since the 1500s is
-                    reproduced below for those interested. Sections 1.10.32 and
-                    1.10.33 from &quot;de Finibus Bonorum et Malorum&quot; by
-                    Cicero are also reproduced in their exact original form,
-                    accompanied by English versions from the 1914 translation by
-                    H. Rackham.
-                </p>
-            </div>
+            <div
+                className='two-col-desc mt:[50px] flex flex-col flex-nowrap gap-[30px] sm:mt-[150px] lg:mt-[80px] lg:flex-row lg:flex-wrap xl:mt-[220px]'
+                dangerouslySetInnerHTML={{
+                    __html: investment?.['investment-section-1'][2]
+                        .field_value as string
+                }}
+            ></div>
             <div className='mt-[72px]'>
                 <div className='text-[40px] font-[700] leading-[38px] small:text-[69px] small:leading-[88px]'>
-                    <h2>
-                        Enquire
-                        <br />
-                        Now
+                    <h2 className='inline-block max-w-[220px] capitalize small:max-w-[270px]'>
+                        {investment?.['investment-section-1'][3].field_value ??
+                            ''}
                     </h2>
                 </div>
                 <EnquireForm />
--- app/sell/page.tsx
+++ app/sell/page.tsx
@@ -1,16 +1,28 @@
-import { EnquireForm } from '@/app/components';
 import Image from 'next/image';
+import { Metadata } from 'next';
+import { EnquireForm } from '@/app/components';
+import { SellYourProperty } from '@/app/types';
+import { fetchData, getFieldValueByName } from '@/app/constants';
+
+const sellYourProperty: SellYourProperty = await fetchData(4);
+
+export async function generateMetadata(): Promise<Metadata> {
+    return {
+        title: sellYourProperty.header[0].metatitle,
+        description: sellYourProperty.header[0].metadescription,
+        keywords: sellYourProperty.header[0].metakeyword
+    };
+}
 
 export default async function SellPage() {
     return (
         <div className='mx-auto mb-[30px] w-full sm:mb-[60px] xl:mb-[84px] 3xl:max-w-[1200px]'>
             <div className='relative mb-[82px]'>
                 <div className='text-[40px] font-[700] leading-[38px] small:text-[69px] small:leading-[88px]'>
-                    <h2>
-                        Sell Your
-                        <br />
-                        Property
-                    </h2>
+                    <h1 className='inline-block max-w-[220px] capitalize small:max-w-[300px]'>
+                        {sellYourProperty?.['sell-your-property-section-1'][0]
+                            .field_value ?? ''}
+                    </h1>
                     <div className='pointer-events-none absolute top-[50%] z-[-30] translate-y-[-30%] scale-[1.4] sm:translate-y-[-60%] md:translate-y-[-60%]  md:scale-[1.3] lg:scale-[1.3] xl:translate-y-[-80%] 2xl:scale-[1.4] msm:translate-y-[-55%] mdlap:translate-y-[-70%]'>
                         <Image
                             src='/images/sell_hero.webp'
@@ -21,32 +33,14 @@ export default async function SellPage() {
                     </div>
                 </div>
             </div>
-            <div className='flex flex-col flex-nowrap gap-[30px] lg:flex-row lg:flex-wrap'>
-                <p className='text-xs leading-[162%] lg:w-[calc(50%-15px)]'>
-                    Contrary to popular belief, Lorem Ipsum is not simply random
-                    text. It has roots in a piece of classical Latin literature
-                    from 45 BC, making it over 2000 years old. Richard
-                    McClintock, a Latin professor at Hampden-Sydney College in
-                    Virginia, looked up one of the more obscure Latin words,
-                    consectetur, from a Lorem Ipsum passage, and going through
-                    the cites of the word in classical literature, discovered
-                    the undoubtable source. Lorem Ipsum comes from sections
-                    1.10.32 and 1.10.33 of &quot;de Finibus Bonorum et
-                    Malorum&quot; (The Extremes of Good and Evil) by Cicero,
-                    written in 45 BC. This book is a treatise on the theory of
-                    ethics, very popular during the Renaissance. The first line
-                    of Lorem Ipsum, &quot;Lorem ipsum dolor sit amet..&quot;,
-                    comes from a line in section 1.10.32.
-                </p>
-                <p className='text-xs leading-[162%] lg:w-[calc(50%-15px)]'>
-                    The standard chunk of Lorem Ipsum used since the 1500s is
-                    reproduced below for those interested. Sections 1.10.32 and
-                    1.10.33 from &quot;de Finibus Bonorum et Malorum&quot; by
-                    Cicero are also reproduced in their exact original form,
-                    accompanied by English versions from the 1914 translation by
-                    H. Rackham.
-                </p>
-            </div>
+            <div
+                className='two-col-desc flex flex-col flex-nowrap gap-[30px] lg:flex-row lg:flex-wrap'
+                dangerouslySetInnerHTML={{
+                    __html: sellYourProperty?.[
+                        'sell-your-property-section-1'
+                    ][2].field_value as string
+                }}
+            ></div>
             <div className='flex flex-col flex-nowrap gap-[0px] lg:flex-row lg:flex-wrap lg:gap-[30px]'>
                 <Image
                     src='/images/sell1.webp'
@@ -65,10 +59,11 @@ export default async function SellPage() {
             </div>
             <div className='mt-[72px]'>
                 <div className='text-[40px] font-[700] leading-[38px] small:text-[69px] small:leading-[88px]'>
-                    <h2>
-                        Enquire
-                        <br />
-                        Now
+                    <h2 className='inline-block max-w-[220px] capitalize small:max-w-[270px]'>
+                        {getFieldValueByName(
+                            sellYourProperty?.['sell-your-property-section-1'],
+                            'Form Heading'
+                        )}
                     </h2>
                 </div>
                 <EnquireForm />
